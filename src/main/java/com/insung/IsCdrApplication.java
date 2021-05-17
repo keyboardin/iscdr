531a38c55b5fa26f67803c39d6cd8b4c246b8ba3
kwak-young-wook <0wook@insunginfo.co.kr>
2021-05-17 17:03:09 +0900

    isStats->isCdr 변경 등

--- src/main/java/com/insung/IsCdrApplication.java
+++ src/main/java/com/insung/IsCdrApplication.java
@@ -6,16 +6,16 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class IsStatsApplication extends SpringBootServletInitializer {
+public class IsCdrApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IsStatsApplication.class, args);
+		SpringApplication.run(IsCdrApplication.class, args);
 	} 
 	
 	//톰켓서버에 war를 배포하기 위해 추가해야하는 코드 
 	@Override 
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) { 
-		return builder.sources(IsStatsApplication.class); 
+		return builder.sources(IsCdrApplication.class); 
 	}
 
 }
